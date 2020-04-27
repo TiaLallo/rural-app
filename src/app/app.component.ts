16b5ac6bf6f4806b178311a09f168098ba2abc98
TiaLallo <tia.lallo@gmail.com>
2020-04-27 10:51:11 +0300

    GoogleAnalytics fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import {Component} from '@angular/core';
+import {NavigationEnd, Router} from '@angular/router';
+import {filter} from 'rxjs/internal/operators/filter';
+
+declare var gtag;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -6,5 +10,15 @@ import {Component} from '@angular/core';
 })
 export class AppComponent {
   title = 'rural-app';
+  constructor(router: Router) {
+    const navEndEvents = router.events.pipe(filter(event => event instanceof NavigationEnd),
+    );
+    navEndEvents.subscribe((event: NavigationEnd) => {
+      gtag('config', 'UA-162432480-1', {
+        page_path: event.urlAfterRedirects
+      });
+    });
+  }
+
 
 }
